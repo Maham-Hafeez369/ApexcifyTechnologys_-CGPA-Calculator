a35e2eabc23453821419ac968f1e8ba901f4d8cf
Maham-Hafeez369 <mahamhafeez369.175@gmail.com>
2025-11-18 18:08:42 +0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 using namespace std;
 class Course
-{
+{ 
 private:
     float credits;
     float grade_point;
 public:
     string name;
-    float getCredits()          //get credits
+    float getCredits() // get credits
     {
         return credits;
     }
-    float getGradePoint()       //get grade point
+    float getGradePoint() // get grade point
     {
         return grade_point;
     }
-    void setCredits(float c)            //set credits
+    void setCredits(float c) // set credits
     {
-         credits=c;
-    } 
-    void setGradePoint(float g)            //set grade point
-    {
-         grade_point = g;
+        credits = c;
     }
-    float getWeightedGradePoint()          //calculate weighted grade point for each course
+    void setGradePoint(float g) // set grade point
+    {
+        grade_point = g;
+    }
+    float getWeightedGradePoint() // calculate weighted grade point for each course
     {
         return credits * grade_point;
     }
@@ -39,18 +39,18 @@ int main()
     for (int i = 0; i < n; i++)
     {
         float credits, grade_point;
-        cout << "Enter course name: ";           
-        cin >> courses[i].name;                                                //input course name
+        cout << "Enter course name: ";
+        cin >> courses[i].name; // input course name
         cout << "Enter credits for " << courses[i].name << ": ";
-        cin >> credits;                                                        //input credits
-        courses[i].setCredits(credits);                                        //set credits
+        cin >> credits;                 // input credits
+        courses[i].setCredits(credits); // set credits
         cout << "Enter grade point for " << courses[i].name << ": ";
-        cin >> grade_point;                                                    //input grade point
-        courses[i].setGradePoint(grade_point);                                 //set grade point
-        total_weighted_grade_points += courses[i].getWeightedGradePoint();    //calculate total weighted grade points
-        total_credits += courses[i].getCredits();                             //calculate total credits
-    }         
-    float cgpa = total_weighted_grade_points / total_credits;                 //calculate CGPA
-    cout << "Your CGPA is: " << cgpa << endl;                                  
+        cin >> grade_point;                                                // input grade point
+        courses[i].setGradePoint(grade_point);                             // set grade point
+        total_weighted_grade_points += courses[i].getWeightedGradePoint(); // calculate total weighted grade points
+        total_credits += courses[i].getCredits();                          // calculate total credits
+    }
+    float cgpa = total_weighted_grade_points / total_credits; // calculate CGPA
+    cout << "Your CGPA is: " << cgpa << endl;
     return 0;
 }
